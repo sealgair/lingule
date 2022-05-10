ba2f9d1adcce86c212a3e1d2cc77a7e5701cfeb2
Chase Caster <sealgair@gmail.com>
2022-05-10 09:01:20 -0500

    serve font files correctly

--- lingule/middleware.py
+++ lingule/middleware.py
@@ -1,4 +1,5 @@
 import os
+from urllib.parse import urlparse
 
 from whitenoise.middleware import WhiteNoiseMiddleware
 from django.conf import settings
@@ -8,6 +9,7 @@ class RootWhiteNoiseMiddleware(WhiteNoiseMiddleware):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.add_file_to_dictionary('/', os.path.join(settings.BASE_DIR, "frontend/build/index.html"))
+        self.add_files(self.font_root, prefix=self.font_prefix)
 
     def process_request(self, request):
         request.path_info = request.path_info.lower()
@@ -15,6 +17,8 @@ class RootWhiteNoiseMiddleware(WhiteNoiseMiddleware):
 
     def configure_from_settings(self, settings):
         super().configure_from_settings(settings)
+        self.font_root = settings.FONT_ROOT
+        self.font_prefix = urlparse(settings.FONT_URL or "").path
         self.static_prefix = "/"
 
     def add_file_to_dictionary(self, url, path, stat_cache=None):
