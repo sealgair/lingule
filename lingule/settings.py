e3b1b0263996bfedabb2cd7c4cf5a3a66190a7ec
Chase Caster <sealgair@gmail.com>
2022-05-13 18:55:01 -0500

    add clock with kiribati time to admin

--- lingule/settings.py
+++ lingule/settings.py
@@ -77,7 +77,9 @@ ROOT_URLCONF = 'lingule.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "templates/"
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
