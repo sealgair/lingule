6582298158e5a77e425928b01af369d23ed70f02
Chase Caster <sealgair@gmail.com>
2022-06-14 22:31:43 -0500

    batch translation requests

--- language/management/commands/translate_languages.py
+++ language/management/commands/translate_languages.py
@@ -1,12 +1,17 @@
 import re
+from itertools import islice
 
 from django.core.management import BaseCommand
-from django.db.models import Q
 from google.cloud import translate_v2 as translate
 
-from i18n.models import LANGUAGE_CHOICES
+from i18n.models import LANGUAGE_CHOICES, Translation
 from language.models import Language
 
+
+def batcher(iterable, batch_size):
+    iterator = iter(iterable)
+    while batch := list(islice(iterator, batch_size)):
+        yield batch
 
 class Command(BaseCommand):
     help = 'Translate language names'
@@ -14,6 +19,7 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('--limit', '-l', dest='limit', type=int)
+        parser.add_argument('--batch-size', '-b', dest='batch_size', type=int, default=100)
         parser.add_argument('--target-language', '-t', dest='target_langs', action='append')
 
     def log(self, msg, level=1):
@@ -21,9 +27,9 @@ class Command(BaseCommand):
             print(msg)
 
     def handle(self, *args, **options):
-        translate_client = translate.Client()
         limit = options.get('limit', None)
         target_langs = options.get('target_langs', [lc for lc, _ in LANGUAGE_CHOICES])
+        batch_size = options.get('batch_size')
         self.verbosity = options.get('verbosity')
 
         languages = Language.objects.all()
@@ -45,19 +51,22 @@ class Command(BaseCommand):
             for k, regex in target_regexes.items()
         }
 
-        self.log(f'translating {languages.count} languages')
-        for obj in languages:
-            self.log(f'translating {obj}', 2)
+        self.log(f'translating {languages.count()} languages')
+        translate_client = translate.Client()
+        for batch in batcher(languages, batch_size):
+            words = [context.format(obj.name) for obj in batch]
             for lc in target_langs:
                 regex = target_regexes[lc]
-                result = translate_client.translate(context.format(obj.name), target_language=lc, source_language='en')
-                match = regex.match(result['translatedText'])
-                self.log(f'\t{lc}...', 3)
-                if match:
-                    value = match.group(1)
-                    self.log(f'\t{value}', 3)
-                else:
-                    value = obj.name
-                    self.log(f"\tcould not parse translation from {result['translatedText']}, falling back to {value}")
-                obj.translations.create(language=lc, value=value.lower())
+                results = translate_client.translate(words, target_language=lc, source_language='en')
+                bulk = []
+                for result, obj in zip(results, batch):
+                    match = regex.match(result['translatedText'])
+                    if match:
+                        value = match.group(1)
+                        self.log(f'{obj}->{lc}: {value}', 3)
+                    else:
+                        value = obj.name
+                        self.log(f"could not parse translation from {result['translatedText']}, falling back to {value}")
+                    bulk.append(Translation(object=obj, language=lc, value=value.lower()))
+                Translation.objects.bulk_create(bulk)
 
